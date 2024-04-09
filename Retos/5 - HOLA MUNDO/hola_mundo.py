65b6aec5ea6bded3cda6cbe539e316b164f58545
David <141661643+davidlopean@users.noreply.github.com>
2024-04-09 14:46:06 +0200

    Create hola_mundo.py

---
+++ Retos/5 - HOLA MUNDO/hola_mundo.py
@@ -0,0 +1,6 @@
+# Se crea la función que imprimirá por pantalla el texto que queramos cuando llamemos a la función.
+def hola_mundo(text):
+    print(text)
+
+#Se llama a la función 'hola_mundo' con el texto 'Hola mundo!'
+hola_mundo("Hola mundo!")
